f8f2b8e54ad1f392e3e35df6e813a4e6da167ce7
417-72KI <417.72ki@gmail.com>
2023-01-05 01:11:29 +0900

    prepare for publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import java.util.*
+
 plugins {
     id(Plugins.kotlin) version Plugins.Version.kotlin
     id(Plugins.dokka) version Plugins.Version.dokka
     id(Plugins.ktlint) version Plugins.Version.ktlint
     `java-library`
     `maven-publish`
+    id(Plugins.gradleNexusPublishPlugin) version Plugins.Version.gradleNexusPublishPlugin
 }
 
 repositories {
@@ -27,5 +30,53 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
     reporters {
         ignoreFailures.set(true)
         reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+    }
+}
+
+// Publish
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn("dokkaJavadoc")
+    archiveClassifier.set("javadoc")
+    from(tasks.named("dokkaJavadoc"))
+}
+
+artifacts {
+    archives(sourcesJar)
+    archives(javadocJar)
+}
+
+group = "io.github.417-72ki"
+version = createVersion(project)
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+val properties = project.localProperties ?: Properties().apply {
+    setProperty("ossrhUsername", System.getenv("OSSRH_USERNAME"))
+    setProperty("ossrhPassword", System.getenv("OSSRH_PASSWORD"))
+    setProperty("sonatypeStagingProfileId", System.getenv("SONATYPE_STAGING_PROFILE_ID"))
+    setProperty("signing.keyId", System.getenv("SIGNING_KEY_ID"))
+    setProperty("signing.password", System.getenv("SIGNING_PASSWORD"))
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(properties.getProperty("ossrhUsername"))
+            password.set(properties.getProperty("ossrhPassword"))
+            stagingProfileId.set(properties.getProperty("sonatypeStagingProfileId"))
+        }
     }
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -19,10 +19,13 @@ object Plugins {
     val kotlin by lazy { "org.jetbrains.kotlin.jvm" }
     val dokka by lazy { "org.jetbrains.dokka" }
     val ktlint by lazy { "org.jlleitschuh.gradle.ktlint" }
+    val gradleNexusPublishPlugin by lazy { "io.github.gradle-nexus.publish-plugin" }
+
 
     object Version {
         val kotlin by lazy { Dependencies.Version.kotlin }
         val dokka by lazy { kotlin }
         val ktlint by lazy { "11.0.0" }
+        val gradleNexusPublishPlugin by lazy { "1.1.0" }
     }
 }
