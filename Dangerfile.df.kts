ace0b1b2b55146e06507e0ae3c03bdb2f0354235
417-72KI <417.72ki@gmail.com>
2023-01-05 02:05:12 +0900

    add Dangerfile

---
+++ Dangerfile.df.kts
@@ -0,0 +1,65 @@
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+@file:DependsOn("io.github.ackeecz:danger-kotlin-junit:0.1.0")
+@file:DependsOn("io.github.417-72ki:danger-kotlin-checkstyle_format:0.0.1-SNAPSHOT")
+@file:OptIn(ExperimentalPathApi::class)
+
+import io.github.ackeecz.danger.junit.JUnitPlugin
+import jp.room417.danger_kotlin_checkstyle_format.CheckstyleFormat
+import systems.danger.kotlin.*
+import java.io.File
+import java.io.IOException
+import java.nio.file.Path
+import kotlin.io.path.*
+
+register plugin JUnitPlugin
+register plugin CheckstyleFormat
+
+danger(args) {
+    val path = Path(System.getProperty("user.dir"))
+
+    path.forEachDirectoryEntryRecursive(glob = "**/build/reports/**/build-error-log.txt") {
+        it.useLines { seq ->
+            seq.forEach { line ->
+                when {
+                    line.startsWith("w: ") -> {
+                        warn(line.removeRange(0, 2))
+                    }
+                    line.startsWith("e: ") -> {
+                        fail(line.removeRange(0, 2))
+                    }
+                }
+            }
+        }
+    }
+    path.forEachDirectoryEntryRecursive(glob = "**/build/reports/ktlint/**/ktlint*.xml") {
+        CheckstyleFormat.report(it)
+    }
+
+    path.forEachDirectoryEntryRecursive(glob = "**/build/test-results/*/*.xml") {
+        JUnitPlugin.parse(File(it.toUri()))
+        JUnitPlugin.report()
+    }
+}
+
+@SinceKotlin("1.5")
+@Throws(IOException::class)
+fun Path.forEachDirectoryEntryRecursive(glob: String, action: (Path) -> Unit) {
+    if (notExists()) return
+    if (!glob.contains("/")) return forEachDirectoryEntry(glob, action)
+    when (val subdir = glob.split("/").first()) {
+        "*" -> forEachDirectoryEntry(glob = "*") {
+            if (it.isDirectory())
+                it.forEachDirectoryEntryRecursive(glob.removePrefix("${subdir}/"), action)
+        }
+        "**" -> {
+            forEachDirectoryEntryRecursive(glob.removePrefix("${subdir}/"), action)
+            forEachDirectoryEntry(glob = "**") {
+                if (it.isDirectory())
+                    it.forEachDirectoryEntryRecursive(glob.removePrefix("${subdir}/"), action)
+            }
+        }
+        else -> resolve(subdir)
+            .forEachDirectoryEntryRecursive(glob.removePrefix("${subdir}/"), action)
+    }
+}
